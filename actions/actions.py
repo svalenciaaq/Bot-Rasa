ec981dee77d71fd9c7953806ce4f72adc4276565
Santiago valencia <sanvalenciaarango@gmail.com>
2021-05-04 02:15:22 -0500

    media changes

--- actions/actions.py
+++ actions/actions.py
@@ -6,13 +6,12 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-
+import pymongo
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
-
+from rasa_sdk.events import SlotSet
+import json
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
@@ -34,5 +33,44 @@ class prueba_action(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        Database.inititialize()
+        entities =  tracker.latest_message['entities']
+        print(entities)
 
-        dispatcher.utter_message(text="Hello World!")
+        
+        data = {
+            "president" : "xd",
+            "lol" : "prueba"
+        }
+        Database.insert("citas",data )
+        dispatcher.utter_message(text="xd")
+      
+        return []
+
+
+
+
+class Database(object):
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
+
+    @staticmethod
+    def inititialize():
+        client = pymongo.MongoClient(Database.URI)
+        print(client)
+        Database.DATABASE = client['prueba']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)    
+       
